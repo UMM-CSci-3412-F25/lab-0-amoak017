28e9f0b532ad3c8a90537a759b5c671f18f68ed3
Mackenzie Amoako <amoak017@umn.edu>
2025-12-08 14:51:55 -0600

    Working on Clean
    making sure clean compiles and runs

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 directory=$1
-archive_name=$(basename "$directory").tgz
-scratch_dir=$(mktemp -directory scratch.XXXXXX)
-clean=cleaned_$(basename "$directory").tgz
+archive_name=$(basename "$directory")
+dir_name="${archive_name%.tgz}"
+scratch_dir=$(mktemp -d scratch.XXXXXX)
+clean=cleaned_$archive_name
 
 # Extract the archive into the temporary directory
-tar -xzf "$directory.tgz" -C "$scratch_dir"
+tar -xzf "$directory" -C "$scratch_dir"
 
 # Find and delete files containing the line "DELETE ME!"
-cd "$scratch_dir/$(basename "$directory")" || exit 1
+cd "$scratch_dir/$dir_name" || exit 1
 grep -rl "DELETE ME!" . | xargs rm -f
-tar -czf "$clean" "$scratch_dir/$(basename "$directory")"
+cd ..
+tar -czf "../../$clean" "$dir_name"
 # in the current directory containing the cleaned contents of the original
 # archive, with the name `cleaned_<original-archive-name>.tgz`.
-mv "$archive_name" ../..
+cd ../..
 # Clean up the temporary directory
 rm -rf "$scratch_dir"
 # If this test fails, you probably didn't name the cleaned archive correctly.
