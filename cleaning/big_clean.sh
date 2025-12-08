96eef658bf7660e15cb3f185537cec7d49eb4760
Mackenzie Amoako <amoak017@umn.edu>
2025-12-08 14:54:07 -0600

    Finishing Clean
    redid clean made it simpler

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-directory=$1
-archive_name=$(basename "$directory")
-dir_name="${archive_name%.tgz}"
-scratch_dir=$(mktemp -d scratch.XXXXXX)
-clean=cleaned_$archive_name
-
-# Extract the archive into the temporary directory
-tar -xzf "$directory" -C "$scratch_dir"
-
-# Find and delete files containing the line "DELETE ME!"
-cd "$scratch_dir/$dir_name" || exit 1
-grep -rl "DELETE ME!" . | xargs rm -f
-cd ..
-tar -czf "../../$clean" "$dir_name"
-# in the current directory containing the cleaned contents of the original
-# archive, with the name `cleaned_<original-archive-name>.tgz`.
-cd ../..
-# Clean up the temporary directory
-rm -rf "$scratch_dir"
-# If this test fails, you probably didn't name the cleaned archive correctly.
+directoryused=$1
+name=$(basename "$directoryused" tgz)
+here=$(pwd)
+scratch=$(mktemp -d)
+tar -xzf "$Directoryused" -c "$scratch"
+grep -rl "DELETE ME!" "$scratch/$name" | xargs rm -f
+cd "$scratch" || exit
+tar -czf "$here/cleaned_$directoryused" "$name"
