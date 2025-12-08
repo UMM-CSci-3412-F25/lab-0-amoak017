19c57bed4f6bf792446d4e9259d5740857278287
Mackenzie Amoako <amoak017@umn.edu>
2025-12-08 14:44:51 -0600

    Finished Compile
    finished code for compile and extract made sure all tests pass.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,5 +2,5 @@
 # $1 is n such that it will run NthPrime with n
 tar -xzf NthPrime.tgz
 cd NthPrime || exit 
-gcc -o nth_prime main.c nth_prime.c
-./nth_prime "$1"
+gcc -o NthPrime main.c nth_prime.c
+./NthPrime "$1"
