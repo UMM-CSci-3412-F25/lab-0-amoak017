c982de76961c426e31012663dfa209c2a229ff4f
Mackenzie Amoako <amoak017@umn.edu>
2025-12-08 14:42:39 -0600

    Continued work on compile
    working on fixing Compile

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # $1 is n such that it will run NthPrime with n
 tar -xzf NthPrime.tgz
-cd NthPrime || exit 1
+cd NthPrime || exit 
 gcc -o nth_prime main.c nth_prime.c
 ./nth_prime "$1"
-mv ../NthPrime.tgz ...
